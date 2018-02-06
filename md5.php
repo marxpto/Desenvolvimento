a7e25012cf3f04a392fcccff3a6007d6f1b30f6e
Marco Aurelio <marco.oliveira.rj@gmail.com>
2018-02-06 19:33:21 -0200

    primeiro commit

---
+++ md5.php
@@ -0,0 +1,11 @@
+
+
+
+<?php
+$string = 'O rato roeu a roupa do rei de Roma';
+$codificada = md5($string);
+echo "O rato roeu a roupa do rei de Roma";
+echo "<br><br>";
+echo "Resultado da codificacao usando md5: " . $codificada;
+// 54cf74d1acdb4037ab956c269b63c8ac
+?>
